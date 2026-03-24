38678a5cf6f5fcddac7166fda63806d46e94280a
Magnus Johannessen <magnusjoh03@gmail.com>
2026-03-24 20:26:56 +0100

    🐛 Fixed backend ping issue

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from app.routers import include_routers
 
 def create_app():
     app = FastAPI()
+    include_routers(app)
     return app
--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -10,27 +10,3 @@ from fastapi_discord.models import GuildPreview
 
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
-ALGORITHM = "HS256"
-
-SCOPES = ["identify", "email"]
-discord = DiscordOAuthClient(
-    settings.DISCORD_CLIENT_ID,
-    settings.DISCORD_CLIENT_SECRET,
-    "http://localhost:8000/auth/callback",
-    SCOPES
-)
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await discord.init()
-    yield
-
-# TODO: Registration using discord oauth2
-@auth_router.get('/auth/login')
-async def login():
-    return {"url": discord.oauth_login_url}
-
-@auth_router.get('/auth/callback')
-async def callback(code: str):
-    token, refresh_token = await discord.get_access_token(code)
-    return {"access_token": token, "refresh_token": refresh_token}
