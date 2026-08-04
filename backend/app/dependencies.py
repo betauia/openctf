a9f40f594e199f4afc1ae9caa2b09e76db2ef223
archrz <Haavard.aleksander.andersen@gmail.com>
2026-08-04 22:59:21 +0200

    restore it my bad

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,8 +1,10 @@
 from app.providers import get_provider
+from app.services.instance_services import InstanceService
 from app.db import SessionLocal
 
 def get_instance_service():
-    return get_provider()
+    provider = get_provider()
+    return InstanceService(provider)
 
 def get_db():
     db = SessionLocal()
